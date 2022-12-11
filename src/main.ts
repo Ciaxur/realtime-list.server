ddfbbfeb5565e8736362311cc2ca84840fd64a91
Omar Omar <oomar@umich.edu>
2022-12-11 14:28:32 -0800

    Add Client/Server TLS

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,23 @@
 require('dotenv').config();
 import { generateHash } from './Utils';
 import * as jwt from 'jsonwebtoken';
+import * as fs from 'fs';
 
 // Env Vars
 const {
   JWT_SECRET,
   CORS_ORIGIN,
+  HTTPS_SERVER_PRIVATE_KEY,
+  HTTPS_SERVER_CERTIFICATE,
+  HTTPS_SERVER_CA,
+  HTTPS_TRUSTED_CLIENT_CA_LIST,
 } = process.env;
 
 // Express & Socket.io Libraries
 import * as express from 'express';
 import * as SocketIO from 'socket.io';
-import { createServer } from 'http';
+import { createServer } from 'https';
+import * as tls from 'tls';
 import {
   IListSchema,
   ListObjectSchema,
@@ -40,6 +46,7 @@ app.use(cors({
   origin: CORS_ORIGIN || '*',
   credentials: true,
 }));
+console.log(`Using CORS_ORIGIN=${CORS_ORIGIN||'*'}`);
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(rateLimit({
@@ -55,10 +62,36 @@ interface IAuthorizedSocket {
     tokenKey: string,
   },
 }
-
-const server = createServer(app);
-const io = SocketIO(server);
 const authorized_conx: IAuthorizedSocket = {};
+
+// Read in TLS server/client certificates.
+const serverPrivateKey = fs.readFileSync(HTTPS_SERVER_PRIVATE_KEY, 'utf8');
+const serverCertificate = fs.readFileSync(HTTPS_SERVER_CERTIFICATE, 'utf8');
+const serverCA = fs.readFileSync(HTTPS_SERVER_CA, 'utf8');
+
+// Read in trusted client CA's to authorize.
+const trustedClientCAs = HTTPS_TRUSTED_CLIENT_CA_LIST
+  .split(',')
+  .map(clientCaPath => fs.readFileSync(clientCaPath, 'utf8'));
+
+// Create an HTTPS server with options.
+const server = createServer({
+  // Server TLS
+  key: serverPrivateKey,
+  cert: serverCertificate,
+
+  // Trusted CAs
+  ca: [
+    serverCA,
+    ...trustedClientCAs,
+  ],
+  requestCert: true,
+
+  // Disable rejecting unauthorized clients to be handled by custom implementation
+  // for logging purposes.
+  rejectUnauthorized: false,
+},app);
+const io = SocketIO(server);
 
 
 // Listen for Socket Events
@@ -186,9 +219,23 @@ io.use((socket, next) => {  // Validate JWT
 });
 
 
-
 // Express Routes
 import Routes from './Routes';
+
+// Server Middleware
+app.use((req, res, next) => {
+  const clientTlsSocket = req.socket as tls.TLSSocket;
+
+  // Verify the authorization of the client.
+  if (!clientTlsSocket.authorized) {
+    console.log(`${Date.now()} [TLS Auth]: Client TLS connection was unauthorized due to:`, clientTlsSocket.authorizationError);
+    return res.status(403).send('Unauthorized user');
+  } else {
+    console.log(`${Date.now()} [TLS Auth]: Client TLS Authorized for`, clientTlsSocket.address());
+  }
+
+  next();
+});
 
 // Register Routes
 app.use('/', Routes);
